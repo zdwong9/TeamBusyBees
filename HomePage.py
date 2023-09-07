f99db208801c0b4163cbf7aae3d9f59095819ad9
zi hui <105930232+ZiHuiC@users.noreply.github.com>
2023-09-08 01:00:23 +0800

    Update HomePage.py

--- HomePage.py
+++ HomePage.py
@@ -31,7 +31,7 @@ if 'past' not in st.session_state:
     st.session_state['past'] = []
 if 'messages' not in st.session_state:
     st.session_state['messages'] = [
-        {"role": "system", "content": "You are a financial market expert."} #maybe can change to a better prompt
+        {"role": "system", "content": "You are a financial market expert."} 
     ]
 
 if "db_client" not in st.session_state:
@@ -89,10 +89,10 @@ def main():
     
 
     with st.sidebar:
-        st.image(image="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Citi.svg/2560px-Citi.svg.png", width=80)
+        st.image(image="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Citi.svg/2560px-Citi.svg.png", width=70)
         response_container = st.container()
         with st.form(key='my_form', clear_on_submit=True):
-            user_input = st.text_area("Ask me a question:", key='input', height=50)
+            user_input = st.text_area("Type your question below:", key='input', height=50)
             submit_button = st.form_submit_button(label='Send')
         if submit_button and user_input:
             output = generate_response(user_input)
@@ -103,18 +103,18 @@ def main():
 
     #reset everything
     if clear_button:
-        st.session_state['generated'] = []
+        st.session_state['generated'] = ["Hi there! Ask me a question."]
         st.session_state['past'] = []
         st.session_state['messages'] = [
-            {"role": "system", "content": "You are a helpful assistant."} #maybe can change to a better prompt
+            {"role": "system", "content": "You are a financial market expert."} 
         ]
 
     #show conversation
     if st.session_state['generated']:
-        with response_container:
-            for i in range(len(st.session_state['generated'])):
-                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
-                message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state["generated"][0], key=str(0), logo=f'https://cdn-icons-png.flaticon.com/512/4712/4712009.png')
+        for i in range(len(st.session_state['past'])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', logo=f'https://cdn-icons-png.flaticon.com/512/2922/2922719.png')
+            message(st.session_state["generated"][i+1], key=str(i+1), logo=f'https://cdn-icons-png.flaticon.com/512/4712/4712009.png')
 
 
 
