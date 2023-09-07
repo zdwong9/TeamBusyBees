56952f49601c4165ff5bddefd09c935e2b4a2b89
zi hui <105930232+ZiHuiC@users.noreply.github.com>
2023-09-08 00:43:06 +0800

    added icons and starting message

--- HomePage.py
+++ HomePage.py
@@ -4,22 +4,23 @@ import openai
 import streamlit as st
 from streamlit_chat import message
 
-load_dotenv()
 # Setting page title and header
 st.set_page_config(page_title="BusyBees", page_icon=":bee:")
 
 # Set API key
+load_dotenv()
 openai.api_key = os.getenv("KEY")
 
 # Initialise session state variables
 if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
+    st.session_state['generated'] = ["Hi there! Ask me a question."]
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 if 'messages' not in st.session_state:
     st.session_state['messages'] = [
-        {"role": "system", "content": "You are a helpful assistant."} #maybe can change to a better prompt
+        {"role": "system", "content": "You are a financial market expert."} #maybe can change to a better prompt
     ]
+
 model = "gpt-3.5-turbo"
 
 # generate a response
@@ -31,20 +32,20 @@ def generate_response(prompt):
         messages=st.session_state['messages']
     )
     response = completion.choices[0].message.content
-    # response = "this is a test response"
+
     st.session_state['messages'].append({"role": "assistant", "content": response})
 
     print(st.session_state['messages'])
     return response
 
-
-
+#sidebar
 with st.sidebar:
-    st.image(image="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Citi.svg/2560px-Citi.svg.png", width=80)
+    st.image(image="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Citi.svg/2560px-Citi.svg.png", width=70)
     # st.sidebar.title("title")
     response_container = st.container()
     with st.form(key='my_form', clear_on_submit=True):
-        user_input = st.text_area("Ask me a question:", key='input', height=50)
+        # user_input = st.text_area("Ask me a question:", key='input', height=50)
+        user_input = st.text_area("", key='input', height=60)
         submit_button = st.form_submit_button(label='Send')
     if submit_button and user_input:
         output = generate_response(user_input)
@@ -53,19 +54,21 @@ with st.sidebar:
     
 clear_button = st.sidebar.button("Clear Conversation", key="clear")
 
-#reset everything
+#clear conversation
 if clear_button:
-    st.session_state['generated'] = []
+    clear_button = False
+    st.session_state['generated'] = ["Hi there! Ask me a question."]
     st.session_state['past'] = []
     st.session_state['messages'] = [
-        {"role": "system", "content": "You are a helpful assistant."} #maybe can change to a better prompt
+        {"role": "system", "content": "You are a financial market expert."} #maybe can change to a better prompt
     ]
 
 #show conversation
 if st.session_state['generated']:
     with response_container:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state["generated"][0], key=str(0), logo=f'https://cdn-icons-png.flaticon.com/512/4712/4712009.png')
+        for i in range(len(st.session_state['past'])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', logo=f'https://cdn-icons-png.flaticon.com/512/2922/2922719.png')
+            message(st.session_state["generated"][i+1], key=str(i+1), logo=f'https://cdn-icons-png.flaticon.com/512/4712/4712009.png')
 
 
